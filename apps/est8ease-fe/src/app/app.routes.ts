4ec544ff55f354052ec3343829a18fa32adbde04
Faisal Choura <faisal.choura@gmail.com>
2024-08-13 19:19:49 +0400

    added redirection

--- apps/est8ease-fe/src/app/app.routes.ts
+++ apps/est8ease-fe/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { PropertiesComponent } from './properties/properties.component';
 import { InterestsComponent } from './interests/interests.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/properties/Emaar_Beach_Front', pathMatch: 'full' },
   { path: 'properties/:id', component: PropertiesComponent },
   { path: 'interests', component: InterestsComponent },
 ];
