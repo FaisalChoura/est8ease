ddb7df0d8046df91a6a2972458c687811c263b11
Faisal Choura <faisal.choura@gmail.com>
2024-11-30 19:10:22 +0400

    added new home screen

--- apps/est8ease-fe/src/app/app.routes.ts
+++ apps/est8ease-fe/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { PropertiesComponent } from './properties/properties.component';
 import { InterestsComponent } from './interests/interests.component';
 import { ClicksComponent } from './clicks/clicks.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/properties/Emaar_Beach_Front', pathMatch: 'full' },
   { path: 'properties/:id', component: PropertiesComponent },
   { path: 'interests', component: InterestsComponent },
   { path: 'clicks', component: ClicksComponent },
+  { path: 'home', component: HomeComponent },
 ];
