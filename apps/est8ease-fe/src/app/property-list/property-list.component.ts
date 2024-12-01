af9fb76a92ceeaa0f10e3974203f70be7788c56b
Faisal Choura <faisal.choura@gmail.com>
2024-12-01 17:29:02 +0400

    added filter toggle to list screen

--- apps/est8ease-fe/src/app/property-list/property-list.component.ts
+++ apps/est8ease-fe/src/app/property-list/property-list.component.ts
@@ -1,29 +1,35 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PropertyFiltersComponent } from '../property-filters/property-filters.component';
 
 @Component({
   selector: 'app-property-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PropertyFiltersComponent],
   templateUrl: './property-list.component.html',
   styleUrl: './property-list.component.scss',
 })
 export class PropertyListComponent {
+  isFilterPanelOpen = false;
   properties = [
     {
       title: 'Spacious 2-bedroom Apartment',
       features: { view: 'Good View', floor: '15th Floor' },
       description: '15th-floor apartment with city skyline views.',
       price: '$1,500/month',
-      size: '900 sqft'
+      size: '900 sqft',
     },
     {
       title: 'Cozy Studio Apartment',
       features: { view: 'Park View', floor: '5th Floor' },
       description: 'Studio apartment with nearby park access.',
       price: '$900/month',
-      size: '450 sqft'
-    }
+      size: '450 sqft',
+    },
     // Add more properties as needed
   ];
+
+  toggleFilterPanel(): void {
+    this.isFilterPanelOpen = !this.isFilterPanelOpen;
+  }
 }
