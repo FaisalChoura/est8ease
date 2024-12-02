d1c0edd66f67d49e779efce59e9d7c8aff4a09a1
Faisal Choura <faisal.choura@gmail.com>
2024-12-02 12:44:58 +0400

    fixed property type

--- apps/est8ease-fe/src/app/property-list/property-list.component.ts
+++ apps/est8ease-fe/src/app/property-list/property-list.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PropertyFiltersComponent } from '../property-filters/property-filters.component';
 import { FilterService } from '../filters.service';
 import { dbService } from '../db.service';
+import { Property } from '../models/property';
 
 @Component({
   selector: 'app-property-list',
@@ -26,23 +27,7 @@ export class PropertyListComponent implements OnInit {
   }
 
   isFilterPanelOpen = false;
-  properties = [
-    {
-      title: 'Spacious 2-bedroom Apartment',
-      features: { view: 'Good View', floor: '15th Floor' },
-      description: '15th-floor apartment with city skyline views.',
-      price: '$1,500/month',
-      size: '900 sqft',
-    },
-    {
-      title: 'Cozy Studio Apartment',
-      features: { view: 'Park View', floor: '5th Floor' },
-      description: 'Studio apartment with nearby park access.',
-      price: '$900/month',
-      size: '450 sqft',
-    },
-    // Add more properties as needed
-  ];
+  properties: Property[] = [];
 
   toggleFilterPanel(): void {
     this.isFilterPanelOpen = !this.isFilterPanelOpen;
